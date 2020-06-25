d566227bc2dc2a535fac4a28fc1d2289a5023668
Cinar Ioan <cinar.ioan@gmail.com>
2020-06-25 17:51:03 +0200

    Some code added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # Catalog App developed by Cinar Ioan
 # Date of creation 25.06.2020
 # New test comment
+from tkinter import *
+
+root = Tk()
+
+myLabel = Label(root, text="Hello World!")
+myLabel2 = Label(root, text="Cinar Ioan")
+myLabel2.grid(row=1, column=1)
+
+myLabel.grid(row=0, column=0)
+
+root.mainloop()
